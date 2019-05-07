d57032f77ae68eb0bb9bff13771b643b82928068
Bart Hertog <Bart@EmbeddedAMS.nl>
2019-05-07 16:54:42 +0200

    Extended the buffer interface.

--- src/MessageBufferInterface.h
+++ src/MessageBufferInterface.h
@@ -15,8 +15,8 @@ namespace EmbeddedProto
   {
     public:
 
-      MessageBuffer() = default;
-      virtual ~MessageBuffer = default;
+      MessageBufferInterface() = default;
+      virtual ~MessageBufferInterface() = default;
 
       //! Delete all data in the buffer.
       virtual void clear() = 0;
@@ -43,12 +43,44 @@ namespace EmbeddedProto
       */
       virtual bool push(const uint8_t* bytes, const uint32_t length) = 0;
 
+      //! Obtain the value of the oldest byte in the buffer.
+      /*!
+          This function will not alter the buffer read index.
+          
+          The parameter byte will not be set if the buffer was empty.
+
+          \param[out] byte When the buffer is not empty this variable will hold the oldest value.
+          \return True when the buffer was not empty.
+      */
+      virtual bool peak(uint8_t& byte) const = 0;
+
       //! Returns the oldest byte in the buffer.
       /*! 
           This function will not alter the buffer read index.
           \warning When the buffer is empty the value returned is undefined.
       */
       virtual uint8_t peak() const = 0;
+
+      //! Advances the internal read index by one when the buffer is not empty.
+      virtual void advance() = 0;
+
+      //! Advances the internal read index by the given value.
+      /*!
+          The advance is limited to the number of bytes in the buffer.
+          \param[in] N The number of bytes to advance the read index.
+      */
+      virtual void advance(const uint32_t N) = 0;
+
+      //! Obtain the value of the oldest byte in the buffer and remove it from the buffer.
+      /*!
+          This function will alter the internal read index.
+          
+          The parameter byte will not be set if the buffer was empty.
+
+          \param[out] byte When the buffer is not empty this variable will hold the oldest value.
+          \return True when the buffer was not empty.
+      */
+      virtual bool pop(uint8_t& byte) const = 0;
 
       //! Returns the oldest byte in the buffer and removes it from the buffer.
       /*!
