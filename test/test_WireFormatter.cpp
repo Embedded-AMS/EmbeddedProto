70b0d6e61439e19f5152a5cd59d230f8cd696a0f
Bart Hertog <Bart@EmbeddedAMS.nl>
2019-09-04 22:48:56 +0200

    SimpleTypes test for deserializing maximum values.

--- test/test_WireFormatter.cpp
+++ test/test_WireFormatter.cpp
@@ -299,4 +299,51 @@ TEST(WireFormatter, SimpleTypes_deserialize_one)
   EXPECT_EQ(1.0F, msg.get_a_float());
 }
 
+TEST(WireFormatter, SimpleTypes_deserialize_max) 
+{
+  InSequence s;
+  Mocks::MessageBufferMock buffer;
+  
+  ON_CALL(buffer, get_size()).WillByDefault(Return(58));
+
+  ::Test_Simple_Types msg;
+
+  uint8_t referee[] = { 0x08, 0xFF, 0xFF, 0xFF, 0xFF, 
+                        0x07, 0x10, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 
+                        0x7F, 0x18, 0xFF, 0xFF, 0xFF, 0xFF, 0x0F, 
+                        0x20, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0x01, 
+                        0x28, 0xFE, 0xFF, 0xFF, 0xFF, 0x0F, 
+                        0x30, 0xFE, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0x01, 
+                        0x38, 0x01, 
+                        0x40, 0x80, 0xA8, 0xD6, 0xB9, 0x07,
+                        0x49, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF,
+                        0x51, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0x7F, 
+                        0x59, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xEF, 0x7F, 
+                        0x65, 0xFF, 0xFF, 0xFF, 0xFF, 
+                        0x6D, 0xFF, 0xFF, 0xFF, 0x7F, 
+                        0x75, 0xFF, 0xFF, 0x7F, 0x7F};
+
+  for(auto r: referee) {
+    EXPECT_CALL(buffer, pop(_)).Times(1).WillOnce(DoAll(SetArgReferee<0>(r), Return(true)));
+  }
+  EXPECT_CALL(buffer, pop(_)).Times(1).WillOnce(Return(false));
+
+  EXPECT_TRUE(msg.deserialize(buffer));
+
+  EXPECT_EQ(std::numeric_limits<int32_t>::max(),  msg.get_a_int32());   
+  EXPECT_EQ(std::numeric_limits<int64_t>::max(),  msg.get_a_int64());     
+  EXPECT_EQ(std::numeric_limits<uint32_t>::max(), msg.get_a_uint32());    
+  EXPECT_EQ(std::numeric_limits<uint64_t>::max(), msg.get_a_uint64());
+  EXPECT_EQ(std::numeric_limits<int32_t>::max(),  msg.get_a_sint32());
+  EXPECT_EQ(std::numeric_limits<int64_t>::max(),  msg.get_a_sint64());
+  EXPECT_EQ(true, msg.get_a_bool());
+  EXPECT_EQ(Test_Enum::TWOBILLION, msg.get_a_enum());
+  EXPECT_EQ(std::numeric_limits<uint64_t>::max(), msg.get_a_fixed64());
+  EXPECT_EQ(std::numeric_limits<int64_t>::max(),  msg.get_a_sfixed64());
+  EXPECT_EQ(std::numeric_limits<double>::max(),   msg.get_a_double());
+  EXPECT_EQ(std::numeric_limits<uint32_t>::max(), msg.get_a_fixed32());
+  EXPECT_EQ(std::numeric_limits<int32_t>::max(),  msg.get_a_sfixed32()); 
+  EXPECT_EQ(std::numeric_limits<float>::max(),    msg.get_a_float());
+}
+
 } // End of namespace test_EmbeddedAMS_WireFormatter
