50db443e3b04c9f512a46f70c538fc6db76727f6
Bart Hertog <Bart@EmbeddedAMS.nl>
2019-11-08 22:59:34 +0100

    Ignore gtest and usr/include files in the code coverages report.

--- code_coverage.sh
+++ code_coverage.sh
@@ -1,4 +1,10 @@
 #! /bin/sh
+
+rm -rf ./code_coverage_report/*
+
 ./build/test/test_EmbeddedProto
-lcov --directory ./build/test --capture --output-file ./code_coverage_report/code_coverage.info -rc lcov_branch_coverage=1
-genhtml ./code_coverage_report/code_coverage.info --branch-coverage --output-directory ./code_coverage_report
+
+lcov --directory ./build/test --capture --output-file ./code_coverage_report/total_code_coverage.info -rc lcov_branch_coverage=1
+lcov --remove ./code_coverage_report/total_code_coverage.info $PWD'/external/googletest/*' '/usr/include/*' -o ./code_coverage_report/filtered_code_coverage.info
+
+genhtml ./code_coverage_report/filtered_code_coverage.info --branch-coverage --output-directory ./code_coverage_report
