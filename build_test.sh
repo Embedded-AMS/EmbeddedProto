64d6b9d277f1e0136b36540cc4850a2737439895
Bart Hertog <Bart@EmbeddedAMS.nl>
2024-05-21 18:36:27 +0200

    For the unit tests. Generate the Embedded Proto code using CMake.

--- build_test.sh
+++ build_test.sh
@@ -32,20 +32,6 @@
 # Fail on first non-zero return code
 set -exuo pipefail
 
-# Generate sources using the EAMS plugin.
-mkdir -p ./build/EAMS
-protoc --plugin=protoc-gen-eams=protoc-gen-eams -I./test/proto --eams_out=./build/EAMS ./test/proto/simple_types.proto
-protoc --plugin=protoc-gen-eams=protoc-gen-eams -I./test/proto --eams_out=./build/EAMS ./test/proto/nested_message.proto
-protoc --plugin=protoc-gen-eams=protoc-gen-eams -I./test/proto --eams_out=./build/EAMS ./test/proto/repeated_fields.proto
-protoc --plugin=protoc-gen-eams=protoc-gen-eams -I./test/proto --eams_out=./build/EAMS ./test/proto/oneof_fields.proto
-protoc --plugin=protoc-gen-eams=protoc-gen-eams -I./test/proto --eams_out=./build/EAMS ./test/proto/include_other_files.proto
-# Delibertly do not manually generate file_to_include.proto and subfolder/file_to_include_from_subfolder.proto 
-# to test the automatic generation of files from including them in include_other_files.proto.
-protoc --plugin=protoc-gen-eams=protoc-gen-eams -I./test/proto --eams_out=./build/EAMS ./test/proto/string_bytes.proto
-protoc --plugin=protoc-gen-eams=protoc-gen-eams -I./test/proto --eams_out=./build/EAMS ./test/proto/empty_message.proto
-protoc --plugin=protoc-gen-eams=protoc-gen-eams -I./test/proto --eams_out=./build/EAMS ./test/proto/optional_fields.proto
-protoc --plugin=protoc-gen-eams=protoc-gen-eams -I./test/proto -I./generator --eams_out=./build/EAMS ./test/proto/field_options.proto
-
 # For validation and testing generate the same message using python
 mkdir -p ./build/python
 mkdir -p ./build/python/subfolder
