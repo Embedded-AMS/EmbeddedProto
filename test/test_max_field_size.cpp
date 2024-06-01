e19dda2d399a4182b6e0d2672a0854fb18392b91
Bart Hertog <Bart@EmbeddedAMS.nl>
2024-06-01 21:57:29 +0200

    Fixed unit tests and added the oneof tests.

--- test/test_max_field_size.cpp
+++ test/test_max_field_size.cpp
@@ -113,33 +113,10 @@ void helper_test(const BASE_TYPE max)
   EXPECT_EQ(buffer.get_size(), FIELD_TYPE::max_serialized_size());
 }
 
-TEST(MaxFieldSize, SimpleTypesMsg_max_serialized_size)
+void helper_set_most_consuming_value(Test_Simple_Types& msg)
 {
-  // First all the basic fields with tag, two enums with tag, and a boolean.
-  //EXPECT_EQ(3*6 + 3*11 + 3*5 + 3*9 + 2*6 + 2, ::Test_Simple_Types::max_serialized_size());
-
-  // Include the length and field tag.
-  //EXPECT_EQ(3*6 + 3*11 + 3*5 + 3*9 + 2*6 + 2 + 1 + 1, ::Test_Simple_Types::max_serialized_size(1));
-
-  helper_test<EmbeddedProto::int32, int32_t>(2147483647);
-  helper_test<EmbeddedProto::int64, int64_t>(-9223372036854775808);
-  helper_test<EmbeddedProto::uint32, uint32_t>(4294967295);
-  helper_test<EmbeddedProto::uint64, uint64_t>(18446744073709551615);
-  helper_test<EmbeddedProto::sint32, int32_t>(2147483647);
-  helper_test<EmbeddedProto::sint64, int64_t>(9223372036854775807);
-  helper_test<EmbeddedProto::fixed32, uint32_t>(4294967295);
-  helper_test<EmbeddedProto::fixed64, uint64_t>(18446744073709551615);
-  helper_test<EmbeddedProto::sfixed32, int32_t>(2147483647);
-  helper_test<EmbeddedProto::sfixed64, int64_t>(9223372036854775807);
-  helper_test<EmbeddedProto::floatfixed, float>(std::numeric_limits<float>::max());
-  helper_test<EmbeddedProto::doublefixed, double>(std::numeric_limits<double>::max());
-  helper_test<EmbeddedProto::boolean, bool>(true);
-
-  Test_Simple_Types msg;
-  EmbeddedProto::WriteBufferFixedSize<150> buffer;
-
   msg.set_a_int32(std::numeric_limits<int32_t>::max());  
-  msg.set_a_int64(std::numeric_limits<int64_t>::max());     
+  msg.set_a_int64(-9223372036854775808);     
   msg.set_a_uint32(std::numeric_limits<uint32_t>::max());    
   msg.set_a_uint64(std::numeric_limits<uint64_t>::max());
   msg.set_a_sint32(std::numeric_limits<int32_t>::max());
@@ -152,6 +129,29 @@ TEST(MaxFieldSize, SimpleTypesMsg_max_serialized_size)
   msg.set_a_fixed32(std::numeric_limits<uint32_t>::max());
   msg.set_a_sfixed32(std::numeric_limits<int32_t>::max()); 
   msg.set_a_float(std::numeric_limits<float>::max());
+
+  msg.set_a_nested_enum(Test_Simple_Types::Nested_Enum::NE_C);
+}
+
+TEST(MaxFieldSize, SimpleTypesMsg_max_serialized_size)
+{
+  helper_test<EmbeddedProto::int32, int32_t>(2147483647);
+  helper_test<EmbeddedProto::int64, int64_t>(-9223372036854775808);
+  helper_test<EmbeddedProto::uint32, uint32_t>(4294967295);
+  helper_test<EmbeddedProto::uint64, uint64_t>(18446744073709551615ull);
+  helper_test<EmbeddedProto::sint32, int32_t>(2147483647);
+  helper_test<EmbeddedProto::sint64, int64_t>(9223372036854775807);
+  helper_test<EmbeddedProto::fixed32, uint32_t>(4294967295);
+  helper_test<EmbeddedProto::fixed64, uint64_t>(18446744073709551615ull);
+  helper_test<EmbeddedProto::sfixed32, int32_t>(2147483647);
+  helper_test<EmbeddedProto::sfixed64, int64_t>(9223372036854775807);
+  helper_test<EmbeddedProto::floatfixed, float>(std::numeric_limits<float>::max());
+  helper_test<EmbeddedProto::doublefixed, double>(std::numeric_limits<double>::max());
+  helper_test<EmbeddedProto::boolean, bool>(true);
+
+  EmbeddedProto::WriteBufferFixedSize<150> buffer;
+  Test_Simple_Types msg;
+  helper_set_most_consuming_value(msg);
 
   msg.serialize(buffer);
   EXPECT_EQ((Test_Simple_Types::max_serialized_size()), buffer.get_size());
@@ -180,23 +180,64 @@ TEST(MaxFieldSize, RepeatedFieldFixedSize_Packed)
 
 TEST(MaxFieldSize, RepeatedFieldFixedSize_Unpacked)
 {
+  EmbeddedProto::RepeatedFieldFixedSize<Test_Simple_Types, 3> rffs;
+  helper_set_most_consuming_value(rffs[0]);
+  helper_set_most_consuming_value(rffs[1]);
+  helper_set_most_consuming_value(rffs[2]);
+
+  EmbeddedProto::WriteBufferFixedSize<500> buffer;
+
   constexpr uint32_t max_ser_size_A = EmbeddedProto::RepeatedFieldFixedSize<Test_Simple_Types, 3>::max_serialized_size(1);
+  rffs.serialize_with_id(1, buffer, false);
+  EXPECT_EQ(buffer.get_size(), max_ser_size_A);
+
+  buffer.clear();
   constexpr uint32_t max_ser_size_B = EmbeddedProto::RepeatedFieldFixedSize<Test_Simple_Types, 3>::max_serialized_size(16);
-  
-  // N times id + data
-  EXPECT_EQ((3*(1 + (3*6 + 3*11 + 3*5 + 3*9 + 2*6 + 2 + 1))), max_ser_size_A);
-  EXPECT_EQ((3*(2 + (3*6 + 3*11 + 3*5 + 3*9 + 2*6 + 2 + 1))), max_ser_size_B);
+  rffs.serialize_with_id(16, buffer, false);
+  EXPECT_EQ(buffer.get_size(), max_ser_size_B);
 }
 
 TEST(MaxFieldSize, OneofMaxFieldSize)
 {
-  EXPECT_EQ(3*6 + 7 + (2 + 1 + 3*6), message_oneof::max_serialized_size());
+  message_oneof msg;
+  msg.set_a(std::numeric_limits<int32_t>::max());
+  msg.set_x(std::numeric_limits<int32_t>::max());
+  msg.set_b(std::numeric_limits<int32_t>::max());
+  msg.set_w(std::numeric_limits<float>::max());
+  msg.mutable_msg_ABC().set_varA(std::numeric_limits<int32_t>::max());
+  msg.mutable_msg_ABC().set_varB(std::numeric_limits<int32_t>::max());
+  msg.mutable_msg_ABC().set_varC(std::numeric_limits<int32_t>::max());
 
-  EXPECT_EQ(25+1+1+1, (string_bytes_oneof<25,1>::max_serialized_size()));
-  EXPECT_EQ(50+1+1+1, (string_bytes_oneof<1,50>::max_serialized_size()));
+  EmbeddedProto::WriteBufferFixedSize<200> buffer;
 
-  EXPECT_EQ(1 + 1 + (3*6 + 7 + (2 + 1 + 3*6)), (combined_oneof<1,1>::max_serialized_size()));
-  EXPECT_EQ((50+1+1+1) + 1 + 1, (combined_oneof<1,50>::max_serialized_size()));
+  msg.serialize(buffer);
+  EXPECT_EQ(buffer.get_size(), message_oneof::max_serialized_size());
+
+  buffer.clear();
+  constexpr uint32_t N_CHARS = 25+1;
+  string_bytes_oneof<N_CHARS, 1> msg_chars;
+  msg_chars.mutable_name().set("ABCDEFGHIJKLMNOPQRSTUVWXYZ");
+  msg_chars.serialize(buffer);
+  EXPECT_EQ(buffer.get_size(), (string_bytes_oneof<N_CHARS,1>::max_serialized_size()));
+  
+  buffer.clear();
+  constexpr uint32_t N_BYTES = 75;
+  string_bytes_oneof<1, N_BYTES> msg_bytes;
+  for(uint32_t i = 0; i < N_BYTES; ++i) { msg_bytes.mutable_data()[i] = 0xFF; }
+  msg_bytes.serialize(buffer);  
+  EXPECT_EQ(buffer.get_size(), (string_bytes_oneof<1,N_BYTES>::max_serialized_size()));
+
+  buffer.clear();
+  combined_oneof<1,1> msg_combiA;
+  msg_combiA.mutable_msg_oneof() = msg;
+  msg_combiA.serialize(buffer);
+  EXPECT_EQ(buffer.get_size(), (combined_oneof<1,1>::max_serialized_size()));
+
+  buffer.clear();
+  combined_oneof<1, N_BYTES> msg_combiB;
+  msg_combiB.mutable_msg_sb_oneof() = msg_bytes; 
+  msg_combiB.serialize(buffer);
+  EXPECT_EQ(buffer.get_size(), (combined_oneof<1,N_BYTES>::max_serialized_size()));
 }
 
 } // End of namespace test_max_field_size
